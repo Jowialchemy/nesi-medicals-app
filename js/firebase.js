01b1446ed77f728927d369ee3b277b6ad2d66752
Jowialchemy <jowialchemystudios@gmail.com>
2026-09-02 12:59:35 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAfi1pRrAWX4LawVfUwCat4c-MFNmue-Rc",
+    authDomain: "nesi-medicals.firebaseapp.com",
+    projectId: "nesi-medicals",
+    storageBucket: "nesi-medicals.firebasestorage.app",
+    messagingSenderId: "242774167260",
+    appId: "1:242774167260:web:76c3f133c970bc7a47ae19"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
