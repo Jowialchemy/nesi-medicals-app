9b2152277ad40237ca48a440517ff46bcd1bc639
Jowialchemy <jowialchemystudios@gmail.com>
2026-09-03 18:00:42 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,56 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { 
+    getAuth,
+    setPersistence,
+    browserLocalPersistence
+} 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyAfi1pRrAWX4LawVfUwCat4c-MFNmue-Rc",
+
     authDomain: "nesi-medicals.firebaseapp.com",
+
     projectId: "nesi-medicals",
+
     storageBucket: "nesi-medicals.firebasestorage.app",
+
     messagingSenderId: "242774167260",
+
     appId: "1:242774167260:web:76c3f133c970bc7a47ae19"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 
+
 const auth = getAuth(app);
 
-export { app, auth };
+
+/*
+   KEEP USER LOGGED IN
+   WHEN MOVING BETWEEN PAGES
+*/
+
+setPersistence(
+    auth,
+    browserLocalPersistence
+).catch((error) => {
+
+    console.error(
+        "Firebase persistence error:",
+        error
+    );
+
+});
+
+
+export {
+    app,
+    auth
+};
